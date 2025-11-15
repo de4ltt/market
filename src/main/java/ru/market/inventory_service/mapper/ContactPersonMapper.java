88ccce940e6f454e197a4e5dee07fac46d7a959b
Aleksej Timko <aleksejtmk@gmail.com>
2025-11-15 19:02:17 +0300

    feat: add counterparty reference to ContactPersonMapper

--- src/main/java/ru/market/inventory_service/mapper/ContactPersonMapper.java
+++ src/main/java/ru/market/inventory_service/mapper/ContactPersonMapper.java
@@ -1,11 +1,16 @@
 package ru.market.inventory_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.market.inventory_service.model.dto.ContactPersonDto;
 import ru.market.inventory_service.model.entity.ContactPerson;
 
 @Mapper(componentModel = "spring")
 public interface ContactPersonMapper {
+
+    @Mapping(target = "counteragentId", source = "counteragent.counteragentId")
     ContactPersonDto toDto(ContactPerson entity);
+
+    @Mapping(target = "counteragent", source = "counteragentId", qualifiedByName = "counteragentById")
     ContactPerson toEntity(ContactPersonDto dto);
 }
