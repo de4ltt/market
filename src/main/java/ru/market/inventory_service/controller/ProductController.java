3276501dfe611752ac4c30807e3b383e4fc57534
Aleksej Timko <aleksejtmk@gmail.com>
2025-11-15 16:25:07 +0300

    feat: add ProductController

---
+++ src/main/java/ru/market/inventory_service/controller/ProductController.java
@@ -0,0 +1,46 @@
+package ru.market.inventory_service.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.market.inventory_service.exception.FailedToRetrieveProductsException;
+import ru.market.inventory_service.model.dto.ProductDto;
+import ru.market.inventory_service.service.ProductService;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/products")
+@AllArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public CompletableFuture<ResponseEntity<List<ProductDto>>> getAllProducts() {
+        return productService.getAllProducts().handle((result, throwable) -> {
+           if (throwable != null)
+               throw new FailedToRetrieveProductsException();
+           else return ResponseEntity.ok(result);
+        });
+    }
+
+    @GetMapping("/{id}")
+    public CompletableFuture<ResponseEntity<ProductDto>> getProductById(@PathVariable Integer id) {
+        return productService.getProductById(id).handle((result, throwable) -> {
+            if (throwable != null)
+                throw new FailedToRetrieveProductsException();
+            else return ResponseEntity.ok(result);
+        });
+    }
+
+    @GetMapping
+    public CompletableFuture<ResponseEntity<List<ProductDto>>> getProductsByQuery(@RequestParam(defaultValue = "") String q) {
+        return productService.getProductsByQuery(q).handle((result, throwable) -> {
+            if (throwable != null)
+                throw new FailedToRetrieveProductsException();
+            else return ResponseEntity.ok(result);
+        });
+    }
+}
